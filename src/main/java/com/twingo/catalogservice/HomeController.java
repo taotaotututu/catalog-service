1cffc670905e35396178a42160c186aa61a6faf7
taotaotututu <1762517761@qq.com>
2023-09-01 17:03:39 +0800

    修改

--- src/main/java/com/twingo/catalogservice/HomeController.java
+++ src/main/java/com/twingo/catalogservice/HomeController.java
@@ -1,12 +1,20 @@
 package com.twingo.catalogservice;
 
+import com.twingo.catalogservice.config.PolarProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+    private final PolarProperties polarProperties;
+
+    public HomeController(PolarProperties polarProperties) {
+        this.polarProperties = polarProperties;
+    }
+
     @GetMapping("/")
     public String getGreeting() {
-        return "Welcome to the book catalog!";
+//        return "Welcome to the book catalog!";
+        return polarProperties.getGreeting();
     }
 }
